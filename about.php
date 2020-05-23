cf2c6f50a04056dede77dd397d488edb38f5ebbd
paragpalod <phpalod@gmail.com>
2020-05-23 22:52:50 +0530

    9. Created About page

---
+++ about.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once 'header.php';?>
+<body>
+
+    <?php require_once 'nav.php';?>
+
+    <div class="brand">The Coffee Shop</div>
+    <div class="address-bar">3481 Balewadi Highstreet | Balewadi, 411033 | +91 1234567890</div>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">About
+                        <strong>The Coffee Shop</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-md-6">
+                    <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/9SjCEOWd3PU' frameborder='0' allowfullscreen></iframe></div>
+                </div>
+                <div class="col-md-6">
+                    <p>
+                      This coffee has so many names; Dalgona Coffee, frothy coffee, whipped Coffee, cloud Coffee, magic coffee and more. No matter how you call it, this cold coffee is AMAZING! This coffee has been viral in Korea during quarantine time, then it got viral on TikTok and now it is very trendy all over the internet.<br>
+                      Dalgona Coffee is a cold latte drink that comes with a velvety smooth and sweet coffee foam on top. The combination of cold milk and the bittersweet coffee is absolutely delicious. Dalgona coffee is inspired by the South Korean dalgona candy, a toffee sponge that resembles the creamy dollop that tops the ice coffee.<br>
+                      So if you want to learn how to make dalgona coffee, follow this easy recipe (only 4 ingredients).
+                    </p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Our
+                        <strong>Team</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="img/abt1.jpg" alt="">
+                    <h3>John Smith
+                        <small>MD</small>
+                    </h3>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="img/abt2.jpg" alt="">
+                    <h3>Anna Smith
+                        <small>CEO</small>
+                    </h3>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="img/abt3.jpg" alt="">
+                    <h3>Jan Smith
+                        <small>COO</small>
+                    </h3>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+    </div>
+
+    <?php require_once 'footer.php';?>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
