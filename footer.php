75c286dcfce8270caf63777a2ddbee238bfacc60
paragpalod <phpalod@gmail.com>
2020-05-23 22:49:56 +0530

    2. Created footer Component

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <p>Copyright &copy; The Coffee Shop 2020</p>
+            </div>
+        </div>
+    </div>
+</footer>
